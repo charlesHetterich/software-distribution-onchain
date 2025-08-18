09bf4a3d2caaaf76dd0f8bb37b0fb3c482f01eed
chetterich <hetterich.charles@gmail.com>
2025-08-17 23:38:34 -0400

    beginning ts project for exploring assethub & relevant chains via papi, & we will also use this to facilitate testing of contract code

---
+++ charles-rnd/src/index.ts
@@ -0,0 +1,54 @@
+// `dot` is the name we gave to `npx papi add`
+import * as DESC from "@polkadot-api/descriptors";
+import { createClient } from "polkadot-api";
+import { getInkClient } from "polkadot-api/ink";
+import { createReviveSdk } from "@polkadot-api/sdk-ink";
+import { getSmProvider } from "polkadot-api/sm-provider";
+import * as SPECS from "polkadot-api/chains";
+import { start } from "polkadot-api/smoldot";
+
+async function initAPIs() {
+    const relayID = "ksmcc3";
+    const paraIDs = ["ksmcc3_asset_hub"] as const;
+
+    const smoldot = start();
+    const relay = await smoldot.addChain({ chainSpec: SPECS.ksmcc3 });
+    const relayClient = createClient(getSmProvider(relay));
+
+    const AHChain = await smoldot.addChain({
+        chainSpec: SPECS.ksmcc3_asset_hub,
+        potentialRelayChains: [relay],
+    });
+    const AHClient = createClient(getSmProvider(AHChain));
+
+    return {
+        kusama: relayClient.getTypedApi(DESC.ksmcc3),
+        kusamaAssetHub: AHClient.getTypedApi(DESC.ksmcc3_asset_hub),
+        crust: null, // TODO!
+    };
+}
+
+async function main() {
+    const APIs = await initAPIs();
+
+    APIs.kusama.event.System.Remarked.watch().subscribe((event) => {
+        console.log("Kusama System.Remarked event:", event);
+    });
+
+    APIs.kusamaAssetHub.event.Revive.ContractEmitted.watch().subscribe(
+        (event) => {
+            console.log("Kusama Asset Hub ContractEmitted event:", event);
+        }
+    );
+
+    // const psp22Sdk = createReviveSdk(DESC.ksmcc3_asset_hub, DESC.contract);
+
+    const contractCodes =
+        await APIs.kusamaAssetHub.query.Revive.PristineCode.getEntries();
+    console.log("Pristine contract codes:");
+    for (const code of contractCodes) {
+        console.log(code.value.asText());
+    }
+}
+
+main().catch(console.error);
